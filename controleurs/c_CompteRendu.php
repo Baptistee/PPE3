a066ab7dab3953a649308a76f40d19f797483ece
Maco13 <36083989+Maco13@users.noreply.github.com>
2018-11-21 15:50:52 +0100

    sexe avec papy commité oui clique sur commite

--- controleurs/c_CompteRendu.php
+++ controleurs/c_CompteRendu.php
@@ -2,16 +2,16 @@
 include("vues/v_sommaire.php");
 //$action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['vis_matricule'];
-switch($_GET['action']){ 
+switch($_GET['action']){
         case 'saisirCR':{
-            include('vues/v_insererCompte.php');
+            include('vues/v_isexe papy.php');
             break;
         }
-	
+
         case 'consulterCR':{
         include('vues/v_insererCompte.php');
-        break;    
+        break;
         }
-        
+
 }
 ?>
