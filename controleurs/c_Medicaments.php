6f262f3a30da5567c36bd19e9998f908328ed232
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-11-21 15:29:13 +0100

    g chenG mon contro rtm

--- controleurs/c_Medicaments.php
+++ controleurs/c_Medicaments.php
@@ -8,17 +8,17 @@ $action = $_REQUEST['action'];
 switch($action){
     case 'liste':{
         $LesMedicaments = $pdo->ObtenirListeMedicament();
-        
-        include("vues/v_MedicamentListe.php");
+
+        include("vues/Medicaments/v_MedicamentListe.php");
         break;
     }
     case 'detail':{
         $id =  $_REQUEST['id'];
         settype($id, "string");
         $unMedicament = $pdo->ObtenirDetailMedicament($id);
-        include("vues/v_MedicamentDetail.php");
+        include("vues/Medicaments/v_MedicamentDetail.php");
         break;
     }
 }
- 
+
 ?>
