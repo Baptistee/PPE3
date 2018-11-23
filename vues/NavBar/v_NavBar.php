81d8c73a9510c0c4208b95c86460d345ad7ea628
Baptiste <baptiste.blct@gmail.com>
2018-11-23 20:00:03 +0100

    correction navbar

--- vues/NavBar/v_NavBar.php
+++ vues/NavBar/v_NavBar.php
@@ -1,15 +1,6 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
 
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-                <img src="./images/logo-gsb.png" width="94" height="59" class="d-inline-block align-top" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav mr-auto">
 
