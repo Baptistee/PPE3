75703865c695ec64b27d80ed153f37009ad835d9
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-11-23 23:30:08 +0100

    mise en place des card sur les tableaux CR et Praticien (reste a regler les tailles dsl)

--- vues/CR/v_consulterCompte.php
+++ vues/CR/v_consulterCompte.php
@@ -1,30 +1,37 @@
-<div class="container">
-<table id="tableaumedicament" class="table table-striped table-bordered" style="width: 100%">
-  <thead>
-    <tr>
-        <th scope="col">Matricule visiteur</th>
-      <th scope="col">Numéro du rapport</th>
-      <th scope="col">Numéro du Praticien</th>
-      <th scope="col">Date du rapport</th>
-      <th scope="col">Bilan du rapport</th>
-      <th scope="col">Motif du rapport</th>
-      <th scope="col">Échantillons</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php foreach ($lesCompteRendu as $key => $value):?>
-        <tr>
-          <th scope="row"><?=$value["VIS_MATRICULE"]?></th>
-          <td><?=$value["RAP_NUM"]?></td>
-          <td><?=$value["PRA_NUM"]?></td>
-          <td><?=$value["RAP_DATE"]?></td>
-          <td><?=$value["RAP_BILAN"]?></td>
-          <td><?=$value["RAP_MOTIF"]?></td>
-          <td><a href="index.php?uc=gererCR&action=details&id=<?= $value["RAP_NUM"]?>">Détail </a></td>
-        </tr>
-       <?php endforeach; ?>
-  </tbody>
-</table>
+<br>
+
+<div class="card mx-auto w-80" style="max-width: 80%;">
+    <div class="card-header text-center"><h4 class="card-title mt-3 text-center">Créer une nouvelle activité</h4></div>
+    <div class="card-body">
+        <article class="card-body mx-auto" style="max-width: 80%;">
+
+        <table id="tableaumedicament" class="table table-striped table-bordered" style="width: 100%">
+          <thead>
+            <tr>
+                <th scope="col">Matricule visiteur</th>
+              <th scope="col">Numéro du rapport</th>
+              <th scope="col">Numéro du Praticien</th>
+              <th scope="col">Date du rapport</th>
+              <th scope="col">Bilan du rapport</th>
+              <th scope="col">Motif du rapport</th>
+              <th scope="col">Échantillons</th>
+            </tr>
+          </thead>
+          <tbody>
+              <?php foreach ($lesCompteRendu as $key => $value):?>
+                <tr>
+                  <th scope="row"><?=$value["VIS_MATRICULE"]?></th>
+                  <td><?=$value["RAP_NUM"]?></td>
+                  <td><?=$value["PRA_NUM"]?></td>
+                  <td><?=$value["RAP_DATE"]?></td>
+                  <td><?=$value["RAP_BILAN"]?></td>
+                  <td><?=$value["RAP_MOTIF"]?></td>
+                  <td><a href="index.php?uc=gererCR&action=details&id=<?= $value["RAP_NUM"]?>">Détail </a></td>
+                </tr>
+               <?php endforeach; ?>
+          </tbody>
+        </table>
+    </div>
 </div>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
 <script>
--- vues/Praticiens/v_consulterPraticien.php
+++ vues/Praticiens/v_consulterPraticien.php
@@ -1,35 +1,42 @@
+<br>
 
+<div class="card mx-auto w-80" style="max-width: 80%;">
+    <div class="card-header text-center"><h4 class="card-title mt-3 text-center">Créer une nouvelle activité</h4></div>
+    <div class="card-body">
+        <article class="card-body mx-auto" style="max-width: 80%;">
 
-    <table id="tablePraticiens" class="table text-center table-striped table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Prenom</th>
-      <th scope="col">Adresse</th>
-      <th scope="col">Code postal</th>
-      <th scope="col">Ville</th>
-      <th scope="col">Coef notoriete</th>
-      <th scope="col">Type de praticien</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php foreach ($lesPraticiens as $key => $value):?>
-        <tr>
-          <th scope="row"><?=$value["PRA_NUM"]?></th>
-          <td><?=$value["PRA_NOM"]?></td>
-          <td><?=$value["PRA_PRENOM"]?></td>
-          <td><?=$value["PRA_ADRESSE"]?></td>
-          <td><?=$value["PRA_CP"]?></td>
-          <td><?=$value["PRA_VILLE"]?></td>
-          <td><?=$value["PRA_COEFNOTORIETE"]?></td>
-          <td><?=$value["TYP_CODE"]?></td>
+        <table id="tablePraticiens" class="table text-center table-striped table-bordered">
+            <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nom</th>
+              <th scope="col">Prenom</th>
+              <th scope="col">Adresse</th>
+              <th scope="col">Code postal</th>
+              <th scope="col">Ville</th>
+              <th scope="col">Coef notoriete</th>
+              <th scope="col">Type de praticien</th>
+            </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($lesPraticiens as $key => $value):?>
+                <tr>
+                  <th scope="row"><?=$value["PRA_NUM"]?></th>
+                  <td><?=$value["PRA_NOM"]?></td>
+                  <td><?=$value["PRA_PRENOM"]?></td>
+                  <td><?=$value["PRA_ADRESSE"]?></td>
+                  <td><?=$value["PRA_CP"]?></td>
+                  <td><?=$value["PRA_VILLE"]?></td>
+                  <td><?=$value["PRA_COEFNOTORIETE"]?></td>
+                  <td><?=$value["TYP_CODE"]?></td>
 
-        </tr>
-       <?php endforeach; ?>
+                </tr>
+               <?php endforeach; ?>
 
-  </tbody>
-</table>
+            </tbody>
+        </table>
+    </div>
+</div>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
  <script>
     $(document).ready(function() {
