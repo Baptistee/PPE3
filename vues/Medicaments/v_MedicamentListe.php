d13283412c2fb16787cd59fcddf31d684aa86934
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-11-23 16:35:36 +0100

    reste

--- vues/Medicaments/v_MedicamentListe.php
+++ vues/Medicaments/v_MedicamentListe.php
@@ -1,4 +1,11 @@
-<table id="tableaumedicament" class="table text-center table-striped table-bordered" style="width: 100%">
+<br>
+
+<div class="card mx-auto w-80" style="max-width: 80%;">
+    <div class="card-header text-center"><h4 class="card-title mt-3 text-center">Créer une nouvelle activité</h4></div>
+    <div class="card-body">
+        <article class="card-body mx-auto" style="max-width: 80%;">
+
+            <table id="tableaumedicament" class="table text-center table-striped table-bordered" style="width: 100%">
   <thead>
     <tr>
       <th scope="col">Dépot légal</th>
@@ -21,15 +28,18 @@
           <td><?=$value["FAM_LIBELLE"]?></td>
           <td><?=$value["MED_PRIXECHANTILLON"]?> €</td>
           <td><a href="index.php?uc=medicament&action=detail&id=<?= $value["MED_DEPOTLEGAL"]?>">Détail </a></td>
-         
+
         </tr>
-       <?php endforeach; ?>    
+       <?php endforeach; ?>
   </tbody>
 </table>
-<script> 
+
+</article>
+</div>
+</div>
+
+<script>
     $(document).ready(function() {
     $('#tableaumedicament').DataTable();
 } );
 </script>
-
-
