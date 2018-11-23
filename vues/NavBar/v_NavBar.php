88d1a5113fe97ec28423445ec70f0badde08634a
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-11-23 16:43:15 +0100

    logo navbar

--- vues/NavBar/v_NavBar.php
+++ vues/NavBar/v_NavBar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
   <a class="navbar-brand" href="#">
-    <img src="./images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    <img src="./images/logo-gsb.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
     Galaxy Swiss Bourdin
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,9 +8,9 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-        <?php 
-        if(isset($_SESSION['login'])){             
-          include("MenuVisiteur.php");  
+        <?php
+        if(isset($_SESSION['login'])){
+          include("MenuVisiteur.php");
         }
         else{
             include("MenuConnection.php");
