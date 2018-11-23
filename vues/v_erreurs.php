413dd710ba9c708027f624ac31d50f2e8d7111a4
Baptiste <baptiste.blct@gmail.com>
2018-11-23 22:50:04 +0100

    incrementation

--- vues/v_erreurs.php
+++ vues/v_erreurs.php
@@ -1,11 +1,16 @@
 <div class ="alert alert-danger">
-<ul>
-<?php 
-if(!empty($_REQUEST['erreurs'])){
-foreach($_REQUEST['erreurs'] as $erreur)
-	{
-      echo "<li>$erreur</li>";
-	}
-}
-?>
-</ul></div>
+	<ul>
+
+		<?php
+
+			if(!empty($_REQUEST['erreurs'])) {
+
+				foreach($_REQUEST['erreurs'] as $erreur) {
+      				echo "<li>$erreur</li>";
+				}
+			}
+
+		?>
+		
+	</ul>
+</div>
