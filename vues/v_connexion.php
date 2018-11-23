2cb90645d715288a45a7881a9a9a53f0103a21d1
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-11-23 16:35:18 +0100

    mespetitstrucs

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -1,7 +1,7 @@
 <div class="container contact-form">
 
     <div class="contact-image">
-        <img src="./images/pinguLogo.png" alt="rocket_contact"/>
+        <img src="./images/MedicamentLogo.png" alt="rocket_contact"/>
     </div>
 
     <form method="post" action="index.php?uc=connexion&action=valideConnexion">
