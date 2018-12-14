dada7f1c87c96dcb71fa534bc01d61ddcc661b8b
lepetitpingouin <45233988+lepetitpingouin@users.noreply.github.com>
2018-12-14 14:22:22 +0100

    Detail médicament responsive

--- vues/Medicaments/v_MedicamentDetail.php
+++ vues/Medicaments/v_MedicamentDetail.php
@@ -6,20 +6,20 @@
         <article class="card-body mx-auto" style="max-width: 80%;">
 
         <div>
-                <label  for="Depot_legal"> DEPOT LEGAL : </label>
-                <?= $unMedicament['MED_DEPOTLEGAL'];?><hr>
-                <label  for="Nom_commercial"> NOM COMMERCIAL : </label>
-                <?= $unMedicament['MED_NOMCOMMERCIAL'];?><hr>
-                <label  for="Famille"> FAMILLE : </label>
-                <?= $unMedicament['FAM_CODE'];?><hr>
-                <label  for="Composition"> COMPOSITION : </label>
-                <?= $unMedicament['MED_COMPOSITION'];?><hr>
-                <label  for="EFFECT"> EFFECT : </label>
-                <?= $unMedicament['MED_EFFETS'];?><hr>
-                <label  for="Contre_Indc"> CONTRE INDC : </label>
-                <?= $unMedicament['MED_CONTREINDIC'];?><hr>
-                <label  for="Contre_Indc"> PRIX ECHANTILLON : </label>
-                <?= $unMedicament['MED_PRIXECHANTILLON'];?> €<hr>
+                <label  for="Depot_legal"> DEPOT LEGAL :
+                <?= $unMedicament['MED_DEPOTLEGAL'];?></label><hr>
+                <label  for="Nom_commercial"> NOM COMMERCIAL :
+                <?= $unMedicament['MED_NOMCOMMERCIAL'];?></label><hr>
+                <label  for="Famille"> FAMILLE :
+                <?= $unMedicament['FAM_CODE'];?></label><hr>
+                <label  for="Composition"> COMPOSITION :
+                <?= $unMedicament['MED_COMPOSITION'];?></label><hr>
+                <label  for="EFFECT"> EFFET :
+                <?= $unMedicament['MED_EFFETS'];?></label><hr>
+                <label  for="Contre_Indc"> CONTRE INDIC :
+                <?= $unMedicament['MED_CONTREINDIC'];?></label><hr>
+                <label  for="Contre_Indc"> PRIX ECHANTILLON :
+                <?= $unMedicament['MED_PRIXECHANTILLON'];?> €</label><hr>
                 <a href="index.php?uc=medicament&action=liste" title="Les médicaments">Retour à la liste</a>
         </div>
 
